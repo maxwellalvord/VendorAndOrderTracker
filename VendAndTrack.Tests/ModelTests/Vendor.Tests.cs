65f609639c9caf2e198c571fc617c4a72a9d2180
Maxwell Alvord <maxwellalvord@gmail.com>
2022-07-22 10:26:55 -0700

    add fourth  passing vender test

--- VendAndTrack.Tests/ModelTests/Vendor.Tests.cs
+++ VendAndTrack.Tests/ModelTests/Vendor.Tests.cs
@@ -54,5 +54,24 @@ namespace VendAndTrack.Tests
       //Assert
       Assert.AreEqual(1, result);
     }
+
+    [TestMethod]
+    public void GetAll_ReturnsAllVendorObjects_VendorList()
+    {
+      //Arrange
+      string name01 = "Work";
+      string name02 = "School";
+      string descript1 = "ball";
+      string descript02 = "dog";
+      Vendor newVendor1 = new Vendor(name01, descript1);
+      Vendor newVendor2 = new Vendor(name02, descript02);
+      List<Vendor> newList = new List<Vendor> { newVendor1, newVendor2 };
+
+      //Act
+      List<Vendor> result = Vendor.GetAll();
+
+      //Assert
+      CollectionAssert.AreEqual(newList, result);
+    }
   }
 }
