363df0937e754b485be39cba5d2b423013346a2c
Maxwell Alvord <maxwellalvord@gmail.com>
2022-07-22 11:28:52 -0700

    add vendor controller

--- VendAndTrack/Controllers/VendorController.cs
+++ VendAndTrack/Controllers/VendorController.cs
@@ -5,7 +5,7 @@ using VendAndTrack.Models;
 
 namespace VendAndTrack.Controllers
 {
-  public class VedorsController : Controller
+  public class VendorsController : Controller
   {
     [HttpGet("/vendors")]
     public ActionResult Index()
@@ -14,6 +14,47 @@ namespace VendAndTrack.Controllers
       return View(allVendors);
     }
 
-    
+    [HttpGet("/vendors/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
+    [HttpPost("/vendors")]
+    public ActionResult Create(string vendorName, string vendorDescription)
+    {
+      Vendor newVendor = new Vendor(vendorName, vendorDescription);
+      return RedirectToAction("Index");
+    }
+
+    [HttpGet("/vendors/{id}")]
+    public ActionResult Show(int id)
+    {
+
+      Dictionary<string, object> model = new Dictionary<string, object>();
+
+      Vendor selectedVendor = Vendor.Find(id);
+
+      List<Order> categoryOrders = selectedVendor.Orders;
+
+      model.Add("vendor", selectedVendor);
+
+      model.Add("order", categoryOrders);
+
+      return View(model);
+    }
+
+    [HttpPost("/vendors/{vendorId}/orders")]
+    public ActionResult Create(int vendorId, string orderDescription, string orderName, string orderPrice)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor foundVendor = Vendor.Find(vendorId);
+      Order newOrder = new Order(orderDescription, orderName, orderPrice);
+      foundVendor.AddOrder(newOrder);
+      List<Order> vendorOrders = foundVendor.Orders;
+      model.Add("order", vendorOrders);
+      model.Add("vendor", foundVendor);
+      return View("Show", model);
+    }
   }
 } 
